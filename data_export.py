4080009b952729c60c0d781cfef0e5821e25fd20
Wenxing Liu <wenxing.liu.09@gmail.com>
2020-02-22 16:42:49 -0700

    summary info

--- data_export.py
+++ data_export.py
@@ -32,7 +32,9 @@ def compute_paired_data(requestees: List[Requestee], log_file: bool = True):
                 "weekday": promised_time_slot.weekday,
                 "volunteer_email_sent": str(requestee.volunteer.email_sent),
                 "email_sent_time_utc": email_sent_time_str,
-                "other_wechat_info": requestee.other_wechat_info
+                "other_wechat_info": requestee.other_wechat_info,
+                "doctor_family": requestee.doctor_family,
+                "patient_family": requestee.patient_family
             }
 
             paired_list.append(paired_info)
@@ -43,6 +45,15 @@ def compute_paired_data(requestees: List[Requestee], log_file: bool = True):
     if log_file:
         file_path = _compute_export_file_path('paired.csv', settings.PAIRING_OUTPUT_DIR)
         paired_df.to_csv(file_path, index=False)
+
+    doctor_family_count = int(paired_df.doctor_family.sum())
+    patient_family_count = int(paired_df.patient_family.sum())
+
+    print(f"""
+    Successful pairs: {len(paired_df)} 
+    Doctor Family: {doctor_family_count}
+    Patient Family: {patient_family_count}
+    """)
 
     return paired_df
 
@@ -121,8 +132,8 @@ def compute_unassgined_requestee(requestees: List[Requestee], log_file: bool = T
             "gender": requestee.gender,
             "preferred_match_gender": requestee.volunteer_gender,
             "english_learning_in_years": requestee.requestee_info.get('english_learning_in_years'),
-            "doctor_family": "Yes" if requestee.doctor_family else "No",
-            "patient_family": "Yes" if requestee.patient_family else "No",
+            "doctor_family": requestee.doctor_family,
+            "patient_family": requestee.patient_family,
             "recommendation_made": "Yes" if requestee.recommendation_made else "No",
             "other_wechat_info": requestee.other_wechat_info
         }
@@ -135,6 +146,15 @@ def compute_unassgined_requestee(requestees: List[Requestee], log_file: bool = T
     if log_file:
         file_path = _compute_export_file_path('unassigned_requestee.csv', settings.DATA_OUTPUT_DIR)
         left_requestee_df.to_csv(file_path, index=False)
+
+    doctor_family_count = int(left_requestee_df.doctor_family.sum())
+    patient_family_count = int(left_requestee_df.patient_family.sum())
+
+    print(f"""
+    Unassigned requests: {len(left_requestee_df)} 
+    Doctor Family: {doctor_family_count}
+    Patient Family: {patient_family_count}
+    """)
 
     return left_requestee_df
 
